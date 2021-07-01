2d9fcdcf0045f827566e0e17f1deb93e47a28ba1
MateusPedrosa <mateus.pedrosa@tidewise.io>
2021-07-01 08:50:26 -0300

    initial commit by rock-create-orogen

---
+++ comms_webrtcTypes.hpp
@@ -0,0 +1,15 @@
+#ifndef comms_webrtc_TYPES_HPP
+#define comms_webrtc_TYPES_HPP
+
+/* If you need to define types specific to your oroGen components, define them
+ * here. Required headers must be included explicitly
+ *
+ * However, it is common that you will only import types from your library, in
+ * which case you do not need this file
+ */
+
+namespace comms_webrtc {
+}
+
+#endif
+
