f8773fcb9c08a9be87213accbbea7e94757c8c5f
Sylvain <sylvain.joyeux@tidewise.io>
2023-02-16 15:36:35 -0300

    test: start rustysignal on a random port that is likely to be available

--- test/webrtc_test.rb
+++ test/webrtc_test.rb
@@ -11,10 +11,13 @@ describe OroGen.comms_webrtc.Task do
 
     before do
         timeout = Time.now + 1
-        @rustysignal_pid = Process.spawn("rustysignal", "127.0.0.1:3012")
+        server = TCPServer.new(0)
+        @rusty_port = server.local_address.ip_port
+        server.close
+        @rustysignal_pid = Process.spawn("rustysignal", "127.0.0.1:#{@rusty_port}")
         while Time.now < timeout
             begin
-                TCPSocket.open("127.0.0.1", 3012)
+                TCPSocket.open("127.0.0.1", @rusty_port)
                 break
             rescue Errno::ECONNREFUSED
                 sleep(0.01)
@@ -27,7 +30,7 @@ describe OroGen.comms_webrtc.Task do
         Process.waitpid @rustysignal_pid
     end
 
-    def deploy(task_a, task_b, time_out, server_name)
+    def deploy(task_a, task_b, time_out, rusty_address = "127.0.0.1:#{@rusty_port}")
         syskit_stub_conf(OroGen.comms_webrtc.Task, task_a.to_s, task_b.to_s)
 
         @task1 = syskit_deploy(
@@ -42,7 +45,7 @@ describe OroGen.comms_webrtc.Task do
         @task1.properties.remote_peer_id = task_b.to_s
         @task1.properties.passive = false
         @task1.properties.data_channel_label = "test_data_label"
-        @task1.properties.signaling_server_name = server_name
+        @task1.properties.signaling_server_name = rusty_address
 
         @task2 = syskit_deploy(
             OroGen.comms_webrtc.Task.with_conf(task_b.to_s).deployed_as(task_b.to_s)
@@ -56,7 +59,7 @@ describe OroGen.comms_webrtc.Task do
         @task2.properties.remote_peer_id = task_a.to_s
         @task1.properties.passive = true
         @task2.properties.data_channel_label = "test_data_label"
-        @task2.properties.signaling_server_name = server_name
+        @task2.properties.signaling_server_name = rusty_address
     end
 
     def configure_and_start()
@@ -76,7 +79,7 @@ describe OroGen.comms_webrtc.Task do
     end
 
     it "emits start only after the data channel is fully established" do
-        deploy("task_a", "task_b", Time.at(2), "127.0.0.1:3012")
+        deploy("task_a", "task_b", Time.at(2))
         output = configure_and_start
 
         assert_equal :DataChannelOpened, output[0].data_channel
@@ -88,7 +91,7 @@ describe OroGen.comms_webrtc.Task do
     end
 
     it "transmits data from the active to the passive task once started" do
-        deploy("task_a", "task_b", Time.at(2), "127.0.0.1:3012")
+        deploy("task_a", "task_b", Time.at(2))
         configure_and_start
 
         data_in = Types.iodrivers_base.RawPacket.new
@@ -103,7 +106,7 @@ describe OroGen.comms_webrtc.Task do
     end
 
     it "transmits data from the passive to the active task once started" do
-        deploy("task_a", "task_b", Time.at(2), "127.0.0.1:3012")
+        deploy("task_a", "task_b", Time.at(2))
         configure_and_start
 
         data_in_task2 = Types.iodrivers_base.RawPacket.new
@@ -118,7 +121,7 @@ describe OroGen.comms_webrtc.Task do
     end
 
     it "successfully reestablishes connection after a stop/start cycle" do
-        deploy("task_a", "task_b", Time.at(2), "127.0.0.1:3012")
+        deploy("task_a", "task_b", Time.at(2))
         configure_and_start
 
         expect_execution do
@@ -129,12 +132,12 @@ describe OroGen.comms_webrtc.Task do
             emit task2.stop_event
         end
 
-        deploy("task_c", "task_d", Time.at(2), "127.0.0.1:3012")
+        deploy("task_c", "task_d", Time.at(2))
         configure_and_start
     end
 
     it "disconnects the WebRTC connection on stop" do
-        deploy("task_a", "task_b", Time.at(2), "127.0.0.1:3012")
+        deploy("task_a", "task_b", Time.at(2))
         configure_and_start
 
         state_task = Types.comms_webrtc.WebRTCState.new
@@ -159,7 +162,7 @@ describe OroGen.comms_webrtc.Task do
     end
 
     it "returns exception when the wait to find the remote peer id exceeds the timeout" do
-        deploy("task_a", "task_b", Time.at(2), "127.0.0.1:3012")
+        deploy("task_a", "task_b", Time.at(2))
         syskit_configure(task1)
 
         output = expect_execution do
@@ -173,7 +176,7 @@ describe OroGen.comms_webrtc.Task do
     end
 
     it "returns exception when the wait for the datachannel exceeds the timeout" do
-        deploy("task_a", "task_b", Time.at(2), "127.0.0.1:3012")
+        deploy("task_a", "task_b", Time.at(2))
         syskit_configure(task2)
 
         output = expect_execution do
@@ -187,7 +190,7 @@ describe OroGen.comms_webrtc.Task do
     end
 
     it "detects that the channel has been closed from the active side" do
-        deploy("task_a", "task_b", Time.at(2), "127.0.0.1:3012")
+        deploy("task_a", "task_b", Time.at(2))
         configure_and_start
 
         output = expect_execution do
@@ -204,7 +207,7 @@ describe OroGen.comms_webrtc.Task do
     end
 
     it "detects that the channel has been closed from the passive side" do
-        deploy("task_a", "task_b", Time.at(2), "127.0.0.1:3012")
+        deploy("task_a", "task_b", Time.at(2))
         configure_and_start
 
         output = expect_execution do
